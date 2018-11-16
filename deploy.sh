b68c09af84da1760a383e97ee088450db44dd16e
Gustavo Brunoro <git@hitnail.net>
2018-11-16 12:05:17 -0200

    updates deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 npm run build.prod
+cp src/index.html dist/
+cp src/*.otf dist/
+cp src/*.ttf dist/
 git subtree split --prefix dist/ -b deploy
 git push -f https://github.com/brunoro/brunoro.github.io.git deploy:master
 git branch -D deploy
