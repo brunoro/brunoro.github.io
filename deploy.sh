7c2d18b5d08ad50d5c5f441972ec1c1ef6d0f1cc
Gustavo Brunoro <git@hitnail.net>
2022-01-03 18:19:22 -0300

    asdf env and latest livecode set

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,5 @@ cp src/index.html dist/
 cp src/*.otf dist/
 cp src/*.ttf dist/
 git subtree split --prefix dist/ -b deploy
-git push -f https://github.com/brunoro/brunoro.github.io.git deploy:master
+git push -f "git@github.com:brunoro/brunoro.github.io.git" deploy:master
 git branch -D deploy
