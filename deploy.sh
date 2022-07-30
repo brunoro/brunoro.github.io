e406a04f19c8af79849a2661a805a870fc7c08c4
Gustavo Brunoro <git@hitnail.net>
2022-07-30 13:32:59 -0300

    fix vite deployment

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 npm run build
-cp src/index.html dist/
-cp src/*.otf dist/
-cp src/*.ttf dist/
 git subtree split --prefix dist/ -b deploy
 git push -f "git@github.com:brunoro/brunoro.github.io.git" deploy:master
 git branch -D deploy
